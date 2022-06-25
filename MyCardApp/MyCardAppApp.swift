04e76f4f26d5581259b845c1dbf218faee2d5e26
temaathletic <greenline1337@gmail.com>
2022-06-25 14:49:56 +0300

    Initial Commit

---
+++ MyCardApp/MyCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardAppApp.swift
+//  MyCardApp
+//
+//  Created by temaathletic on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
